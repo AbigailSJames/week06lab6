f2748838f3bd046c8eff3336178424686d038f2d
818736 <818736@SAIT224157IT.ACDM.DS.SAIT.CA>
2020-10-24 00:02:25 -0600

    Week 6 Lab - Shopping List Added all the functionality

--- src/java/Servlets/ShoppingListServlet.java
+++ src/java/Servlets/ShoppingListServlet.java
@@ -1,86 +1,88 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author 818736
  */
-public class ShoppingListServlet extends HttpServlet {
+public class ShoppingListServlet extends HttpServlet 
+{
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ShoppingListServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ShoppingListServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       String action = request.getParameter("action");
+       HttpSession session = request.getSession();
+       
+       if(action == null || action.equals(""))
+       {
+         getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+       }
+       else if(action.equals("logout"))
+        {
+          session.invalidate();
+          session = request.getSession();
+          getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        }
+       else
+       {   
+           String username = request.getParameter("username");
+           session = request.getSession();
+           session.setAttribute("username", username);
+           getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);  
+
+       }
+         
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+            throws ServletException, IOException
+    {
+        String username = request.getParameter("username");
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+        String action = request.getParameter("action");
+        ArrayList<String> shopList = (ArrayList<String>) session.getAttribute("shoplist");
+        
+        switch(action)
+        {
+           
+            case "add":
+                if (shopList == null)
+                {
+                    shopList = new ArrayList<>();
+                    String item = request.getParameter("listItem");
+                    shopList.add(item);
+                    session.setAttribute("shoplist", shopList);
+                    getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+                }
+                else
+                {
+                     String item = request.getParameter("listItem");
+                     shopList.add(item);
+                     session.setAttribute("shoplist", shopList);
+                     getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+                }
+            break;
+            case "delete":
+                String radio = request.getParameter("listItems");
+                shopList.remove(shopList.indexOf(radio));
+                session.setAttribute("shoplist", shopList);
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+            break;
+        }
+        
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
--- web/WEB-INF/shoppingList.jsp
+++ web/WEB-INF/shoppingList.jsp
@@ -5,13 +5,34 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Shopping List</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Shopping List</h1>        
+
+        Hello, ${username} <a href="ShoppingList?action=logout">Logout</a>
+        <h2>List</h2>
+        <form method="post" action="ShoppingList">
+            Add item: <input type="text" name="listItem" value="${item}">
+            <input name="action" value="add" type="hidden">
+            <input type="submit" value="Add">
+        </form>
+        
+             <form method="post" action="ShoppingList">
+                 <ul>
+                     <input type="hidden" name="counter" value="0">
+                     <c:forEach items="${shoplist}" var="item">
+                         <li><input type="radio" name="listItems" value="${item}">${item}</input></li>
+                     </c:forEach>
+                     
+                 </ul>
+            <input name="action" value="delete" type="hidden">
+            <input type="submit" value="Delete">
+        </form>
     </body>
 </html>
